0c8ae3e03a87fe294444c618d815afb4f3c2e26e
yaya2127 <kinetibebyared@gmail.com>
2026-08-31 19:26:11 +0300

    test(chaos): add automated pod kill failure injection simulation test

--- js/app.js
+++ js/app.js
@@ -574,3 +574,4 @@ Audit Compliance Sign-off: ______________________________________
 <!-- aug31_surge_commit_1 -->
 <!-- aug31_surge_commit_2 -->
 <!-- aug31_surge_commit_3 -->
+<!-- aug31_surge_commit_4 -->
